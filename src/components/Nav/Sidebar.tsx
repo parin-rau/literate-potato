59a92fa644672fcc4a71b7979024d4018319b777
Parin <parin.rau@gmail.com>
2023-10-10 13:54:46 -0700

    Resize top navbar to go under sidebar (i.e. home button instead of search bar in top left corner)

--- src/components/Nav/Sidebar.tsx
+++ src/components/Nav/Sidebar.tsx
@@ -54,7 +54,7 @@ function SidebarContent({
 
 export default function Sidebar() {
 	return (
-		<div className="fixed z-10 top-0 left-0 h-screen w-32 m-0 p-2 pt-16 hidden sm:flex flex-col gap-2 dark:bg-neutral-900 bg-slate-100 border-r dark:border-r-neutral-800 border-r-slate-200">
+		<div className="fixed z-20 top-0 left-0 h-screen w-32 m-0 p-2 hidden sm:flex flex-col gap-2 dark:bg-neutral-900 bg-slate-100 border-r dark:border-r-neutral-800 border-r-slate-200">
 			<SidebarContent />
 		</div>
 	);
