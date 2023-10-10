b2a3cfa8cdd1300b70944c11a1c34e687cf0c813
Parin <parin.rau@gmail.com>
2023-10-09 17:44:51 -0700

    Refactor authController mongoDB services to separate auth service layer file to prepare for Postgres integration to backend

--- src/auth/controllers/authController.ts
+++ src/auth/controllers/authController.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from "express";
-import "dotenv/config";
-import * as mongoDB from "mongodb";
-import { connectToDatabase } from "../../db/mongodb";
-import { formatRegistration, validateLogin } from "../userValidation";
-import { Login, UserToken, Register } from "../../types";
-import jwt from "jsonwebtoken";
-import * as EmailValidator from "email-validator";
+//import "dotenv/config";
+//import * as mongoDB from "mongodb";
+//import { connectToDatabase } from "../../db/mongodb";
+//import { formatRegistration, validateLogin } from "../userValidation";
+import { Login, Register } from "../../types";
+//import jwt from "jsonwebtoken";
+//import * as EmailValidator from "email-validator";
+import * as authService from "../services/mongodb/auth";
 
-const localUsers = process.env.LOCAL_USERS ?? "users";
+//const localUsers = process.env.LOCAL_USERS ?? "users";
 
 export async function createNewUser(req: Request, res: Response) {
 	const data: {
@@ -15,156 +16,180 @@ export async function createNewUser(req: Request, res: Response) {
 		form: Register;
 	} = await req.body;
 
-	if (data.kind !== "register")
-		return res.status(400).send({ message: "Not a registration request" });
+	const { status, message } = await authService.registerUser(data);
 
-	const isValidEmail = EmailValidator.validate(data.form.email);
-	if (!isValidEmail)
-		return res
-			.status(401)
-			.send({ message: "Invalid email address format" });
+	return res.status(status).send(message);
 
-	if (data.form.password !== data.form.passwordConfirm)
-		return res.status(401).send({ message: "Passwords do not match" });
+	// if (data.kind !== "register")
+	// 	return res.status(400).send({ message: "Not a registration request" });
 
-	const client: mongoDB.MongoClient = await connectToDatabase();
-	const db: mongoDB.Db = client.db(process.env.VITE_LOCAL_DB);
-	const coll: mongoDB.Collection = db.collection(localUsers);
-	const isNewUser =
-		(await coll.countDocuments({
-			$or: [{ username: data.form.username }, { email: data.form.email }],
-		})) === 0;
+	// const isValidEmail = EmailValidator.validate(data.form.email);
+	// if (!isValidEmail)
+	// 	return res
+	// 		.status(401)
+	// 		.send({ message: "Invalid email address format" });
 
-	const newUserData = await formatRegistration(data);
+	// if (data.form.password !== data.form.passwordConfirm)
+	// 	return res.status(401).send({ message: "Passwords do not match" });
 
-	if (isNewUser && newUserData) {
-		try {
-			const result = await coll.insertOne(newUserData);
-			await client.close();
-			res.status(200).send(result);
-		} catch (e) {
-			console.error(e);
-			res.status(401).send({ message: "Unable to register new user" });
-		}
-	} else {
-		await client.close();
-		res.status(401).send({
-			message: "Username or email already registered",
-		});
-	}
+	// const client: mongoDB.MongoClient = await connectToDatabase();
+	// const db: mongoDB.Db = client.db(process.env.VITE_LOCAL_DB);
+	// const coll: mongoDB.Collection = db.collection(localUsers);
+	// const isNewUser =
+	// 	(await coll.countDocuments({
+	// 		$or: [{ username: data.form.username }, { email: data.form.email }],
+	// 	})) === 0;
+
+	// const newUserData = await formatRegistration(data);
+
+	// if (isNewUser && newUserData) {
+	// 	try {
+	// 		const result = await coll.insertOne(newUserData);
+	// 		await client.close();
+	// 		res.status(200).send(result);
+	// 	} catch (e) {
+	// 		console.error(e);
+	// 		res.status(401).send({ message: "Unable to register new user" });
+	// 	}
+	// } else {
+	// 	await client.close();
+	// 	res.status(401).send({
+	// 		message: "Username or email already registered",
+	// 	});
+	// }
 }
 
 export async function loginUser(req: Request, res: Response) {
-	try {
-		const data: { kind: "login"; form: Login } = await req.body;
+	const data: { kind: "login"; form: Login } = await req.body;
 
-		if (data.kind !== "login") {
-			return res.status(400).send({ message: "Not a login request" });
-		}
+	const { status, success, message, accessToken, cookie } =
+		await authService.loginUser(data);
 
-		const client: mongoDB.MongoClient = await connectToDatabase();
-		const db: mongoDB.Db = client.db(process.env.VITE_LOCAL_DB);
-		const coll: mongoDB.Collection = db.collection(localUsers);
-
-		const storedUser = await coll.findOne({ username: data.form.username });
-
-		if (!storedUser) {
-			await client.close();
-			return res
-				.status(401)
-				.send({ message: "Incorrect username or password" });
-		}
-
-		const userVerified = await validateLogin(data, storedUser.password);
-
-		if (!userVerified)
-			return res
-				.status(401)
-				.send({ message: "Incorrect username or password" });
-
-		if (!process.env.ACCESS_JWT_SECRET || !process.env.REFRESH_JWT_SECRET)
-			return res.status(500).send({ message: "Missing secrets" });
-
-		const user: UserToken = {
-			username: storedUser.username,
-			userId: storedUser.userId,
-			roles: storedUser.roles,
-		};
-		const accessToken = jwt.sign(user, process.env.ACCESS_JWT_SECRET, {
-			expiresIn: "15s",
-		});
-		const refreshToken = jwt.sign(user, process.env.REFRESH_JWT_SECRET, {
-			expiresIn: "1d",
-		});
-
-		const addRefreshToken = await coll.updateOne(
-			{ userId: storedUser.userId },
-			{ $set: { refreshToken } }
-		);
-
-		await client.close();
-
-		if (addRefreshToken.acknowledged) {
-			return res
-				.status(200)
-				.cookie("refreshToken", refreshToken, {
-					httpOnly: true,
-					sameSite: "lax",
-					secure: process.env.PROD ? true : false,
-					maxAge: 24 * 60 * 60 * 1000,
-				})
-				.json({ accessToken });
-		} else {
-			return res.status(500).send({ message: "Something went wrong" });
-		}
-	} catch (e) {
-		console.error(e);
+	if (!success || !accessToken || !cookie) {
+		return res.status(status).json(message);
+	} else {
+		return res
+			.status(status)
+			.cookie(cookie.name, cookie.val, cookie.options)
+			.json({ accessToken });
 	}
+
+	// try {
+	// 	const data: { kind: "login"; form: Login } = await req.body;
+
+	// 	if (data.kind !== "login") {
+	// 		return res.status(400).send({ message: "Not a login request" });
+	// 	}
+
+	// 	const client: mongoDB.MongoClient = await connectToDatabase();
+	// 	const db: mongoDB.Db = client.db(process.env.VITE_LOCAL_DB);
+	// 	const coll: mongoDB.Collection = db.collection(localUsers);
+
+	// 	const storedUser = await coll.findOne({ username: data.form.username });
+
+	// 	if (!storedUser) {
+	// 		await client.close();
+	// 		return res
+	// 			.status(401)
+	// 			.send({ message: "Incorrect username or password" });
+	// 	}
+
+	// 	const userVerified = await validateLogin(data, storedUser.password);
+
+	// 	if (!userVerified)
+	// 		return res
+	// 			.status(401)
+	// 			.send({ message: "Incorrect username or password" });
+
+	// 	if (!process.env.ACCESS_JWT_SECRET || !process.env.REFRESH_JWT_SECRET)
+	// 		return res.status(500).send({ message: "Missing secrets" });
+
+	// 	const user: UserToken = {
+	// 		username: storedUser.username,
+	// 		userId: storedUser.userId,
+	// 		roles: storedUser.roles,
+	// 	};
+	// 	const accessToken = jwt.sign(user, process.env.ACCESS_JWT_SECRET, {
+	// 		expiresIn: "15s",
+	// 	});
+	// 	const refreshToken = jwt.sign(user, process.env.REFRESH_JWT_SECRET, {
+	// 		expiresIn: "1d",
+	// 	});
+
+	// 	const addRefreshToken = await coll.updateOne(
+	// 		{ userId: storedUser.userId },
+	// 		{ $set: { refreshToken } }
+	// 	);
+
+	// 	await client.close();
+
+	// 	if (addRefreshToken.acknowledged) {
+	// 		return res
+	// 			.status(200)
+	// 			.cookie("refreshToken", refreshToken, {
+	// 				httpOnly: true,
+	// 				sameSite: "lax",
+	// 				secure: process.env.PROD ? true : false,
+	// 				maxAge: 24 * 60 * 60 * 1000,
+	// 			})
+	// 			.json({ accessToken });
+	// 	} else {
+	// 		return res.status(500).send({ message: "Something went wrong" });
+	// 	}
+	// } catch (e) {
+	// 	console.error(e);
+	// }
 }
 
 export async function logoutUser(req: Request, res: Response) {
 	const cookies = req.cookies;
 
-	if (!cookies) return res.sendStatus(204);
-	if (!("refreshToken" in cookies) || !cookies.refreshToken)
-		return res.sendStatus(204);
-	const refreshToken = cookies.refreshToken;
-
-	const client: mongoDB.MongoClient = await connectToDatabase();
-	const db: mongoDB.Db = client.db(process.env.VITE_LOCAL_DB);
-	const coll: mongoDB.Collection = db.collection(localUsers);
-	const foundUser = await coll.findOne({ refreshToken });
-
-	if (!foundUser) {
-		await client.close();
-		return res
-			.clearCookie("refreshToken", {
-				httpOnly: true,
-				sameSite: "lax",
-				secure: process.env.PROD ? true : false,
-				maxAge: 24 * 60 * 60 * 1000,
-			})
-			.sendStatus(204);
-	}
-
-	await coll.updateOne(
-		{ refreshToken },
-		{
-			$unset: {
-				refreshToken: "",
-				gracePeriodToken: "",
-				gracePeriodEnd: "",
-			},
-		}
-	);
-	await client.close();
+	const { status, clearCookie } = await authService.logoutUser(cookies);
 
 	return res
-		.clearCookie("refreshToken", {
-			httpOnly: true,
-			sameSite: "lax",
-			secure: process.env.PROD ? true : false,
-			maxAge: 24 * 60 * 60 * 1000,
-		})
-		.sendStatus(204);
+		.clearCookie(clearCookie.name, clearCookie.options)
+		.sendStatus(status);
+
+	// if (!cookies) return res.sendStatus(204);
+	// if (!("refreshToken" in cookies) || !cookies.refreshToken)
+	// 	return res.sendStatus(204);
+	// const refreshToken = cookies.refreshToken;
+
+	// const client: mongoDB.MongoClient = await connectToDatabase();
+	// const db: mongoDB.Db = client.db(process.env.VITE_LOCAL_DB);
+	// const coll: mongoDB.Collection = db.collection(localUsers);
+	// const foundUser = await coll.findOne({ refreshToken });
+
+	// if (!foundUser) {
+	// 	await client.close();
+	// 	return res
+	// 		.clearCookie("refreshToken", {
+	// 			httpOnly: true,
+	// 			sameSite: "lax",
+	// 			secure: process.env.PROD ? true : false,
+	// 			maxAge: 24 * 60 * 60 * 1000,
+	// 		})
+	// 		.sendStatus(204);
+	// }
+
+	// await coll.updateOne(
+	// 	{ refreshToken },
+	// 	{
+	// 		$unset: {
+	// 			refreshToken: "",
+	// 			gracePeriodToken: "",
+	// 			gracePeriodEnd: "",
+	// 		},
+	// 	}
+	// );
+	// await client.close();
+
+	// return res
+	// 	.clearCookie("refreshToken", {
+	// 		httpOnly: true,
+	// 		sameSite: "lax",
+	// 		secure: process.env.PROD ? true : false,
+	// 		maxAge: 24 * 60 * 60 * 1000,
+	// 	})
+	// 	.sendStatus(204);
 }
---
+++ src/auth/services/mongodb/auth.ts
@@ -0,0 +1,226 @@
+import { CookieOptions } from "express";
+import "dotenv/config";
+import * as mongoDB from "mongodb";
+import { connectToDatabase } from "../../../db/mongodb";
+import { formatRegistration, validateLogin } from "../../userValidation";
+import { Login, UserToken, Register } from "../../../types";
+import jwt from "jsonwebtoken";
+import * as EmailValidator from "email-validator";
+
+const localUsers = process.env.LOCAL_USERS ?? "users";
+
+export async function registerUser(data: { kind: "register"; form: Register }) {
+	const res: { status: number; success: boolean; message?: string } = {
+		status: 500,
+		success: false,
+	};
+
+	if (data.kind !== "register") {
+		res.status = 400;
+		res.message = "Not a registration request";
+		return res;
+	}
+
+	const isValidEmail = EmailValidator.validate(data.form.email);
+	if (!isValidEmail) {
+		res.status = 401;
+		res.message = "Invalid email address format";
+		return res;
+	}
+
+	if (data.form.password !== data.form.passwordConfirm) {
+		res.status = 401;
+		res.message = "Passwords do not match";
+		return res;
+	}
+
+	const client: mongoDB.MongoClient = await connectToDatabase();
+	const db: mongoDB.Db = client.db(process.env.VITE_LOCAL_DB);
+	const coll: mongoDB.Collection = db.collection(localUsers);
+	const isNewUser =
+		(await coll.countDocuments({
+			$or: [{ username: data.form.username }, { email: data.form.email }],
+		})) === 0;
+
+	const newUserData = await formatRegistration(data);
+
+	if (isNewUser && newUserData) {
+		try {
+			const result = await coll.insertOne(newUserData);
+			await client.close();
+
+			res.status = 201;
+			res.message = "Registered new user";
+			res.success = result.acknowledged;
+			return res;
+		} catch (e) {
+			console.error(e);
+			res.status = 401;
+			res.message = "Unable to register new user";
+			return res;
+		}
+	} else {
+		await client.close();
+		res.status = 401;
+		res.message = "Username or email already registered";
+		return res;
+	}
+}
+
+export async function loginUser(data: { kind: "login"; form: Login }) {
+	const res: {
+		status: number;
+		success: boolean;
+		message?: string;
+		accessToken?: string;
+		cookie?: { name: string; val: string; options: CookieOptions };
+	} = {
+		status: 500,
+		success: false,
+	};
+
+	try {
+		if (data.kind !== "login") {
+			res.status = 400;
+			res.message = "Not a login request";
+			return res;
+		}
+
+		const client: mongoDB.MongoClient = await connectToDatabase();
+		const db: mongoDB.Db = client.db(process.env.VITE_LOCAL_DB);
+		const coll: mongoDB.Collection = db.collection(localUsers);
+
+		const storedUser = await coll.findOne({ username: data.form.username });
+
+		if (!storedUser) {
+			await client.close();
+
+			res.status = 401;
+			res.message = "Incorrect username or password";
+			return res;
+		}
+
+		const userVerified = await validateLogin(data, storedUser.password);
+
+		if (!userVerified) {
+			res.status = 401;
+			res.message = "Incorrect username or password";
+			return res;
+		}
+
+		if (!process.env.ACCESS_JWT_SECRET || !process.env.REFRESH_JWT_SECRET) {
+			res.status = 500;
+			res.message = "Missing secrets";
+			return res;
+		}
+
+		const user: UserToken = {
+			username: storedUser.username,
+			userId: storedUser.userId,
+			roles: storedUser.roles,
+		};
+		const accessToken = jwt.sign(user, process.env.ACCESS_JWT_SECRET, {
+			expiresIn: "15s",
+		});
+		const refreshToken = jwt.sign(user, process.env.REFRESH_JWT_SECRET, {
+			expiresIn: "1d",
+		});
+
+		const addRefreshToken = await coll.updateOne(
+			{ userId: storedUser.userId },
+			{ $set: { refreshToken } }
+		);
+
+		await client.close();
+
+		if (addRefreshToken.acknowledged) {
+			res.status = 200;
+			res.success = addRefreshToken.acknowledged;
+			res.accessToken = accessToken;
+			res.cookie = {
+				name: "refreshToken",
+				val: refreshToken,
+				options: {
+					httpOnly: true,
+					sameSite: "lax",
+					secure: process.env.PROD ? true : false,
+					maxAge: 24 * 60 * 60 * 1000,
+				},
+			};
+			return res;
+		} else {
+			res.message = "Something went wrong";
+			return res;
+		}
+	} catch (e) {
+		console.error(e);
+		return res;
+	}
+}
+
+export async function logoutUser(cookies: Record<string, unknown>) {
+	const res: {
+		status: number;
+		success: boolean;
+		clearCookie: { name: string; options: CookieOptions };
+	} = {
+		status: 500,
+		success: false,
+		clearCookie: {
+			name: "refreshToken",
+			options: {
+				httpOnly: true,
+				sameSite: "lax",
+				secure: process.env.PROD ? true : false,
+				maxAge: 24 * 60 * 60 * 1000,
+			},
+		},
+	};
+
+	if (!cookies) {
+		res.status = 204;
+		res.success = true;
+		return res;
+	}
+
+	if (!("refreshToken" in cookies) || !cookies.refreshToken) {
+		res.status = 204;
+		res.success = true;
+		return res;
+	}
+
+	const refreshToken = cookies.refreshToken;
+
+	try {
+		const client: mongoDB.MongoClient = await connectToDatabase();
+		const db: mongoDB.Db = client.db(process.env.VITE_LOCAL_DB);
+		const coll: mongoDB.Collection = db.collection(localUsers);
+		const foundUser = await coll.findOne({ refreshToken });
+
+		if (!foundUser) {
+			await client.close();
+
+			res.status = 204;
+			return res;
+		}
+
+		const updateUser = await coll.updateOne(
+			{ refreshToken },
+			{
+				$unset: {
+					refreshToken: "",
+					gracePeriodToken: "",
+					gracePeriodEnd: "",
+				},
+			}
+		);
+		await client.close();
+
+		res.status = 204;
+		res.success = updateUser.acknowledged;
+		return res;
+	} catch (e) {
+		console.error(e);
+		return res;
+	}
+}
